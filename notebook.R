c04a91a6b41a48a01e4c1b9e64cdff66abd4e761
Maria Vittoria Vestini <113996044+MaviVestini@users.noreply.github.com>
2023-05-08 19:50:30 +0200

    Nested

--- notebook.R
+++ notebook.R
@@ -9,21 +9,18 @@ set.seed(seed)
 # Libraries and Data ---------------------------------------------------------------
 library(glmnet)
 library(NMOF)
+library(parallel)
+library(snow)
 
 test_set_vero <- read.csv("test.csv")
 train_set <- read.csv("train.csv")
 
 
+quant <- range(quantile(train_set$y, c(0.25, 0.75)))
+Lower <- quant[1] - 1.5*(diff(quant))
+Upper <- quant[2] + 1.5*(diff(quant))
 
-# Parameters --------------------------------------------------------------
-k <- c(5, 9)
-d_grid <- c(1, 3) 
-q_grid <- seq(3, 20, 1)
-#positions <- c(0.3,0.4,0.5,0.7, .9)
-lambdas <- 10^seq(-3, -1, .5)
-alphas <- c(0, 1)
-
-
+train_set <- train_set[(train_set$y > Lower)& (train_set$y < Upper),]
 
 # Functions ---------------------------------------------------------------
 
@@ -56,6 +53,8 @@ cross_val_func <- function(x){
   k <- x[3]
   a <- x[4]
   l <- x[5] 
+  #p <- x[6]
+  
   # size of the fold
   l_folds <- nrow(train_set) / k 
   score <- rep(NA, k)
@@ -65,7 +64,7 @@ cross_val_func <- function(x){
     cv_train <- train_set[-idx,]
     
     
-    knots <- seq(0, 1 , length.out=q)
+    knots <- seq(1/q, 1 , length.out=q)
     
     M_cv_train <- power_functions(d = d, q = q, knots = knots, x = cv_train$x)
     # M_cv_train <- data.frame(M_cv_train , target = cv_train$y)
@@ -83,16 +82,115 @@ cross_val_func <- function(x){
 }
 
 
+nested_crossval <- function(x){
+  d <- x[1]
+  q <- x[2]
+  K <- x[3]
+  a <- x[4]
+  l <- x[5] 
+  #p <- x[6]
+  R <- 100
+  l_folds <<- nrow(train_set) / K
+  
+  #es <- c()
+  a_list <- rep(NA, R*K)
+  b_list <- rep(NA, R*K)
+  for(r in (1:R)){
+    idx <- sample((1:nrow(train_set)),nrow(train_set))
+    
+    for(k in (1:K)){  
+      cv_test <- train_set[idx[((k-1)*l_folds+1): (k*l_folds)],]
+      cv_train <- train_set[-idx[((k-1)*l_folds+1): (k*l_folds)],]
+      
+      # inner cross
+      e_in <- inner_crossval(x, cv_train)
+      
+      # Outer cross
+      if(q == 0) knots <- c()
+      else knots <- seq(1/q, 0.8, length.out=q)
+     
+      M_cv_train <- power_functions(d = d, q = q, knots = knots, x = cv_train$x)
+      M_cv_test <-  power_functions(d = d, q = q, knots = knots, x = cv_test$x)
+      
+      cv_model <- glmnet(M_cv_train, cv_train$y,family = "gaussian", alpha=a, lambda=l)
+      cv_predictions <- predict(cv_model, M_cv_test)
+      
+      e_out <- (cv_test$y-cv_predictions)^2
+      
+      a_list[(r-1)*K+k] <- (mean(e_in)-mean(e_out))^2 
+      b_list[(r-1)*K+k] <- (sd(e_out)^2)/l_folds
+      #es <- c(es, e_in)   
+    }
+  }
+  
+  mse <- mean(a_list)-mean(b_list)
+  #err <- mean(es)
+  return(mse)
+}
 
-# Grid Search -------------------------------------------------------------
+inner_crossval <- function(x, train_set){
+  d <- x[1]
+  q <- x[2]
+  K <- x[3]
+  a <- x[4]
+  l <- x[5] 
+  #p <- x[6]
+  
+  e_in <- c()
+  
+  for(k in (1:(K-1))){
+    idx <- ((k-1)*l_folds+1): (k*l_folds)
+    cv_test <- train_set[idx,]
+    cv_train <- train_set[-idx,]
+    
+    if(q == 0) knots <- c()
+    else knots <- seq(1/q, 0.8, length.out=q)
+    
+    M_cv_train <- power_functions(d = d, q = q, knots = knots, x = cv_train$x)
+    M_cv_test <-  power_functions(d = d, q = q, knots = knots, x = cv_test$x)
+    
+    cv_model <- glmnet(M_cv_train, cv_train$y,family = "gaussian", alpha=a, lambda=l)
+    cv_predictions <- predict(cv_model, M_cv_test)
+    
+    e_temp <- (cv_test$y-cv_predictions)^2
+    e_in <- c(e_in, e_temp)
+  }
+  
+  return(e_in)
+}
+
+
+aux <- nested_crossval(c(1,10,4,0,0.1))
+
+# Parameters -------------------------------------------------------------
+
+k <- c(4)
+d_grid <- c(1, 3) 
+q_grid <- seq(8, 12, 1)
+#positions <- c(0.3, 0.4, 0.5, 0.7)
+lambdas <- 10^seq(-2.5, -1.5, .25)
+alphas <- seq(0, 1)
 
 # Set the parameter for the CV
 parameters <- list(d_grid, q_grid, k, alphas, lambdas)
 
+# CV vanilla --------------------------------------------------------------
 # Select the best combination of parameters
-res <- gridSearch(cross_val_func, levels=parameters)
+cl = makeCluster(detectCores())
+clusterExport(cl, c('train_set', 'power_functions', 'glmnet'))
+res <- gridSearch(cross_val_func, levels=parameters, method = 'snow', cl=cl)
+stopCluster(cl)
 best_params <- res$minlevels
+names(best_params) <- c('d', 'q', 'k', 'alpha', 'lambda')
 
+
+# CV nested --------------------------------------------------------------
+cl = makeCluster(detectCores())
+clusterExport(cl, c('train_set', 'inner_crossval', 'power_functions', 'glmnet'))
+res <- gridSearch(nested_crossval, levels=parameters, method = 'snow', cl=cl)
+stopCluster(cl)
+best_params <- res$minlevels
+names(best_params1) <- c('d', 'q', 'k', 'alpha', 'lambda')
 
 
 # Prediction --------------------------------------------------------------
@@ -103,9 +201,10 @@ q_best <- best_params[2]
 k_best <- best_params[3]
 a_best <- best_params[4]
 l_best <- best_params[5]
+#p_best <- best_params[6]
 
 # Compute the predictions
-knots <- seq(0, 1, length.out = q_best)
+knots <- seq(1/q_best, 0.8, length.out=q_best)
 M_train <- power_functions(d = d_best, q = q_best, knots = knots, x = train_set$x)
 M_test <- power_functions( d = d_best, q = q_best, knots = knots, x = test_set_vero$x)
 knots_test <- power_functions( d = d_best, q = q_best, knots = knots, x = knots)
@@ -139,17 +238,17 @@ points(knots, predict(final_model, knots_test), col='red', pch=3, cex=1, lwd=4)
 # plot_fun <- function(x_min, x_max){
 #   ggplot() +
 #     geom_point(aes(x = train_set$x, y = train_set$y, color = 'Real Data'), size = green_point_size, shape=16) +
-#     geom_point(aes(x = test_set_vero$x, y = predictions, color = 'Predicted'), size = blue_point_size) + 
-#     geom_point(aes(x = knots, y = predict(final_model, knots_test), color = 'Knots'), shape = 4, stroke = 1.7, size = red_cross_size) + 
+#     geom_point(aes(x = test_set_vero$x, y = predictions, color = 'Predicted'), size = blue_point_size) +
+#     geom_point(aes(x = knots, y = predict(final_model, knots_test), color = 'Knots'), shape = 4, stroke = 1.7, size = red_cross_size) +
 #     theme_minimal() +
 #     labs(x = "x", y = "y", color = "Legend", title = 'Prediction on WMAP data', shape = "", color="") +
 #     scale_color_manual(values = colors) +
-#     theme(legend.title = element_text(size=12), legend.text = element_text(size=11), plot.title = element_text(hjust = 0.5)) + 
+#     theme(legend.title = element_text(size=12), legend.text = element_text(size=11), plot.title = element_text(hjust = 0.5)) +
 #     coord_cartesian(xlim = c(x_min, x_max))+
 #     guides(color = guide_legend(override.aes=list(shape = c(4, 16, 16), size = 2)))}
 # 
 # manipulate(plot_fun(x.min, x.max), x.min = slider(0,.9, 0, step = .1), x.max = slider(.1, 1, 1, step = .1))
-
+# 
 
 
 
